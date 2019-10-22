9291720e16ff049c424850586e8664fa1cf211dc
xut <394024347@qq.com>
2019-10-22 08:42:18 +0800

    JSX first test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React,{Component} from "react"
 class App extends Component{
   render() {
     return (
-    	<div>
-		  hello aha~
-		</div>
-	)
+    	//JSX
+    	<ul className={"my-list"}>
+		  <li>{1+1>3?'aha.com':'哇哦哦'}</li>
+		  <li>do u like react</li>
+		</ul>
+	);
+    //render的实质
+	// var child1=React.createElement("li",null,'aha.com');
+	// var child2 = React.createElement("li",null,'d u like react');
+	// var root=  React.createElement('ul',{className:'my-list'},child1,child2);
+
   }
 }
 
